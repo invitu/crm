0d74506d5adee12141ae40237803f276f72bf11b
Vehiana Tepuhiarii <vehi@invitu.com>
2025-01-14 15:05:16 -1000

    [ADD] Add crm_lead_approval module

---
+++ crm_lead_approval/models/crm_lead.py
@@ -0,0 +1,32 @@
+# Copyright 2024 INVITU SARL
+# License AGPL-3 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = "crm.lead"
+
+    approval = fields.Selection(
+        [
+            ("none", "None"),
+            ("done", "Approve"),
+            ("normal", "Reserved"),
+            ("blocked", "Disapprove"),
+        ],
+        default="none",
+        tracking=True,
+        readonly=True,
+    )
+    to_be_approved = fields.Boolean(
+        string="To be approved",
+        compute="_compute_to_be_approved",
+        help="Indicates whether the record required approval or not",
+    )
+
+    def _compute_to_be_approved(self):
+        for record in self:
+            record.to_be_approved = (
+                record.team_id in self.env.company.approval_team_ids
+                or record.tag_ids in self.env.company.approval_tag_ids
+            )
